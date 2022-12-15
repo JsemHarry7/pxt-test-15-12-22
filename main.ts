886a65af5e18add471bbe5182d155abbc6731582
Harry <115696199+JsemHarry7@users.noreply.github.com>
2022-12-15 11:31:36 +0100

    last commit

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
-let score = 0
 let kostka = 0
-basic.forever(function(){
+let score = 0
+// basic.showNumber(3)
+// basic.pause(200)
+// basic.showNumber(2)
+// basic.pause(200)
+// basic.showNumber(1)
+// basic.pause(200)
     input.onShake(function(){
         let kostka = randint(1,6)   
     if (kostka == 1){
@@ -11,8 +16,9 @@ basic.forever(function(){
             . . . . .
             . . . . .
             `)
-        score + 1
-        music.ringTone(Note.C)
+        score += 1
+        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        basic.pause(500)
     }
     else if (kostka == 2){
         basic.showLeds(`
@@ -22,8 +28,9 @@ basic.forever(function(){
             . . . . .
             . . . . .
             `)
-        score + 1
-        music.ringTone(Note.C)
+        score += 1
+        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        basic.pause(500)
     }
     else if (kostka == 3){
         basic.showLeds(`
@@ -33,9 +40,45 @@ basic.forever(function(){
             . . . # .
             . . . . .
             `)
-        score + 1
-        music.ringTone(Note.C)
+        score += 1
+        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    else if (kostka == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        score += 1
+        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    else if (kostka == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        score += 1
+        music.playTone(Note.C, music.beat(BeatFraction.Whole))
+        basic.pause(500)
+    }
+    else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+        music.playTone(Note.A, music.beat(BeatFraction.Whole))
+        whaleysans.showNumber(score)
+        basic.pause(2500)
+        score = 0
     }
     })
-    
-})
