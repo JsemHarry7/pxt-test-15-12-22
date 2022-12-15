eb29e207f3883a7c00b442d97062bacd3cca4c33
Harry <115696199+JsemHarry7@users.noreply.github.com>
2022-12-15 11:06:00 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-
+let score = 0
+let kostka = 0
+basic.forever(function(){
+    input.onShake(function(){
+        let kostka = randint(1,6)   
+    if (kostka == 1){
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        score + 1
+        music.ringTone(Note.C)
+    }
+    else if (kostka == 2){
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+        score + 1
+        music.ringTone(Note.C)
+    }
+    else if (kostka == 3){
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+        score + 1
+        music.ringTone(Note.C)
+    }
+    })
+    
+})
